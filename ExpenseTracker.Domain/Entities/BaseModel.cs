de4f8f90997a4fa85e2da40c5e5815d31d56c4d1
aviworking <mwaifungad@gmail.com>
2022-07-02 11:00:16 +0200

    Created BaseModel

---
+++ ExpenseTracker.Domain/Entities/BaseModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ExpenseTracker.Domain.Entities
+{
+    public class BaseModel
+    {
+        [Column(TypeName = "smalldatetime")]
+        public DateTime? DateCreated { get; set; }
+
+        [Column(TypeName = "smalldatetime")]
+        public DateTime? DateModified { get; set; }
+    }
+}
