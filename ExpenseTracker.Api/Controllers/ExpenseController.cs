6cf33226bc5458acdb86d3ea643b174f707cc564
aviworking <mwaifungad@gmail.com>
2022-07-05 18:13:10 +0200

    Created ReadExpensesByKey endpoint in ExpenseController.

--- ExpenseTracker.Api/Controllers/ExpenseController.cs
+++ ExpenseTracker.Api/Controllers/ExpenseController.cs
@@ -40,5 +40,31 @@ namespace ExpenseTracker.Api.Controllers
                 return StatusCode(StatusCodes.Status500InternalServerError);
             }
         }
+
+        /// <summary>
+        /// URL: http://localhost:6600/api/expense-tracker/expenses/{key}
+        /// </summary>
+        /// <param name="key">Primary key of expense entity.</param>
+        [HttpGet]
+        [Route(RouteConstants.Expenses + "{key}")]
+        public async Task<IActionResult> ReadExpenseByKey(int key) 
+        {
+            try
+            {
+                if (key <= 0)
+                    return StatusCode(StatusCodes.Status400BadRequest);
+
+                var expense = await context.Expenses.FindAsync(key);
+
+                if (expense == null)
+                    return StatusCode(StatusCodes.Status404NotFound);
+
+                return Ok(expense);
+            }
+            catch
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+        }
     }
 }
